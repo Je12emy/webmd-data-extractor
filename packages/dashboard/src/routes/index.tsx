ace05bbd91b324bc49f08d183d2fc24bce72266f
Jeremy Zelaya <jeremyzelaya@hotmail.es>
2025-09-15 17:53:31 -0600

    feat: select board

--- packages/dashboard/src/routes/index.tsx
+++ packages/dashboard/src/routes/index.tsx
@@ -1,5 +1,6 @@
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select";
 import { createFileRoute } from "@tanstack/react-router";
-import logo from "../logo.svg";
+import data from "../../../../config.json";
 
 export const Route = createFileRoute("/")({
 	component: App,
@@ -9,30 +10,18 @@ function App() {
 	return (
 		<div className="text-center">
 			<header className="min-h-screen flex flex-col items-center justify-center bg-[#282c34] text-white text-[calc(10px+2vmin)]">
-				<img
-					src={logo}
-					className="h-[40vmin] pointer-events-none animate-[spin_20s_linear_infinite]"
-					alt="logo"
-				/>
-				<p>
-					Edit <code>src/routes/index.tsx</code> and save to reload.
-				</p>
-				<a
-					className="text-[#61dafb] hover:underline"
-					href="https://reactjs.org"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn React
-				</a>
-				<a
-					className="text-[#61dafb] hover:underline"
-					href="https://tanstack.com"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
-					Learn TanStack
-				</a>
+				<Select>
+					<SelectTrigger className="w-[180px]">
+						<SelectValue placeholder="Team Board" />
+					</SelectTrigger>
+					<SelectContent>
+						{data.boards.map(({ id, name }) => (
+							<SelectItem key={id} value={name}>
+								{name}
+							</SelectItem>
+						))}
+					</SelectContent>
+				</Select>
 			</header>
 		</div>
 	);
